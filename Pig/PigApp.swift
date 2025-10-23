74642b4192f1a1031a4b1202df4285f170a1288c
Edwin Tovar <poohwinnie2800@gmail.com>
2025-10-23 16:19:28 -0700

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Edwin Tovar on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
